d8630bb469429294c45fe6f238558ac174b3315d
Felipe Randolfi <fdrandolfi@gmail.com>
2024-10-05 20:52:43 -0300

    update locals

--- database/firebase/config.js
+++ database/firebase/config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNHIAYotNy6pq5bH7YF6uGjfMq96pYX38",
-  authDomain: "compositiontools-db.firebaseapp.com",
-  projectId: "compositiontools-db",
-  storageBucket: "compositiontools-db.appspot.com",
-  messagingSenderId: "398044584974",
-  appId: "1:398044584974:web:0370267076e0ec4cfc305f",
-  measurementId: "G-XBN55KY6M2"
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
 
 const config = initializeApp(firebaseConfig);
 
